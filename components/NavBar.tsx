873f6e161b96ec31c5a6e732690b0125a393a4de
Dung Vuong <dungcoder1213@gmail.com>
2023-01-24 15:05:32 -0800

    post video

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -13,7 +13,8 @@ import useAuthStore from '../store/authStore'
 
 const NavBar = () => {
     const user = false
-    const {userProfile, addUser} = useAuthStore()
+    const {userProfile, addUser, removeUser} = useAuthStore()
+
 
     return (
         <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
@@ -33,7 +34,39 @@ const NavBar = () => {
             <div>
                 {userProfile
                     ? (
-                        <div>{userProfile.userName}</div>
+                        <div className='flex gap-5 md:gap-10'>
+                            <Link href="/upload">
+                                <button className='border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2'>
+                                    <IoMdAdd className='text-xl' />{' '}
+                                    <span className='hidden md:block'>Upload </span>
+                                </button>
+                            </Link>
+                            {userProfile.image && (
+                                <Link href='/'>
+                                    <>
+                                        <div>
+                                            <Image
+                                                className='rounded-full cursor-pointer'
+                                                src={userProfile.image}
+                                                alt='user'
+                                                width={40}
+                                                height={40}
+                                            />
+                                        </div>
+                                    </>
+                                </Link>
+                            )}
+                            <button 
+                                type='button'
+                                className='border-2 p-2 rounded-full cursor-pointer outline-none shadow-md'
+                                onClick={() => {
+                                    googleLogout()
+                                    removeUser()
+                                }}
+                            >
+                                <AiOutlineLogout color='red' fontSize={21}/>
+                            </button>
+                        </div>
                     ) 
                     : (
                         <GoogleLogin
--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['www.localsyr.com']
+    domains: ['www.localsyr.com', 'lh3.googleusercontent.com']
   }
 }
 
